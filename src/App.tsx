091a3c05ef8ae37cc59d05bc2cc556c7fdcd64d5
JessicaHoang <jessica97hoang@gmail.com>
2021-09-17 20:43:13 -0700

    check if edits go through

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Grid, GridItem, Box } from "@chakra-ui/react"
-import { ChakraProvider } from "@chakra-ui/react"
+import { 
+  ChakraProvider, 
+  Grid,
+  Box } from "@chakra-ui/react"
 import './App.css';
 
 function App() {
   return (
-    <div>
       <ChakraProvider>
         This is Text
         <Grid templateColumns="repeat(5, 1fr)" gap={6}>
@@ -16,9 +16,16 @@ function App() {
         <Box w="100%" h="10" bg="blue.500" />
         </Grid>
     </ChakraProvider>
-    </div>
-    
   );
 }
 
 export default App;
+
+// import React from 'react';
+// function App() {
+//   return (
+//     <div className="App">
+//       My App
+//     </div>
+//   );
+// }
